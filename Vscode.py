524a6eccc5103624da2cf7b27ef3e66ce45152a2
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-01-18 14:28:09 +0300

    Python Works

---
+++ Vscode.py
@@ -0,0 +1,11 @@
+# Task 1
+# Converting Celsius to Fahrenheit
+temp_value = int(input("Enter Celsius value : "))
+result = ((float(temp_value) * 1.8) + 32.0)  # From formula F = ((C*1.8)+32)
+print("Fahrenheit :", int(result))
+
+# Task 2
+# Converting Kilometer to Mile
+lenght = int(input("Enter Kilometer value : "))
+result = (float(lenght) * 0.621)
+print("Miles :", int(result))
